393fc5ff046ae7877efa2f6747a330eb8bdeb733
Ghori, Moinatik <MOINATIK_GHORI@homedepot.com>
2021-12-13 21:51:35 -0500

    Solution_2 is added

--- src/solution_1.py
+++ src/solution_1.py
@@ -21,7 +21,7 @@ def findMaxBudget(n, arr):
     maxBudgte = arr[0] * n
 
     # Comparing maxBuddget with other elements, if found maxBudget will be udpated.
-    for i in range(1,n):
+    for i in range(n):
         if maxBudgte < (arr[i] * (n-i)):
             maxBudgte = arr[i] * (n-i)
 
